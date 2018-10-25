2b821ad9ecbb9e85cec633af85c32da31c1f5c09
milukiriu2010 <milu.kiriu2010@gmail.com>
2018-10-25 20:31:15 +0900

    bracket

--- app/src/main/java/milu/kiriu2010/calv2/ContextCal.kt
+++ app/src/main/java/milu/kiriu2010/calv2/ContextCal.kt
@@ -1,16 +1,13 @@
 package milu.kiriu2010.calv2
 
+import android.util.Log
 import java.lang.NumberFormatException
 import java.math.BigDecimal
 import java.util.*
 
-class ContextCal {
-    // 計算式
-    var eq: String = ""
+class ContextCal(
     // トークンリスト
-    val tokenLst: MutableList<String> = mutableListOf()
-    // 計算式を解析した位置
-    var pos: Int = 0
+    val tokenLst: MutableList<String> = mutableListOf() ) {
 
     enum class CalType {
         // 未確定
@@ -27,6 +24,8 @@ class ContextCal {
         OPERATOR_MULTI,
         // 除算
         OPERATOR_DIV,
+        // 累乗
+        OPERATOR_POW,
         // 左かっこ"(",
         BRACKET_LEFT,
         // 右かっこ")",
@@ -39,19 +38,27 @@ class ContextCal {
         NUM
     }
 
-    constructor(eq: String) {
-        this.eq = eq
-
-        val tokenizer = StringTokenizer(this.eq,"+-*/^()",true)
+    constructor(eq: String) : this() {
+        val tokenizer = StringTokenizer(eq,"+-*/^()",true)
         while ( tokenizer.hasMoreTokens() ) {
             tokenLst.add( tokenizer.nextToken() )
         }
+    }
+
+    fun toEquation(): String {
+        return tokenLst.joinToString(separator = "")
     }
 
     @Throws(CalException::class)
     fun execute(): BigDecimal {
         // 符号(正負)を数値へマージ
         calSignMerge()
+
+        // かっこ"()"内の計算を実施
+        calBracket()
+
+        // 累乗計算を実施
+        calPow()
 
         // 乗除算を実施
         calMultiDiv()
@@ -143,63 +150,100 @@ class ContextCal {
             index++
         }
 
+    }
 
-        /*
-        tokenLst.forEachIndexed { index, s ->
-            when  {
-                // プラス符号
-                ( s == "+" ) -> {
-                    // 式の先頭 or カッコ内の先頭
-                    if ( ( prevCalType == CalType.YET ) or ( prevCalType == CalType.BRACKET_LEFT ) ) {
-                        prevCalType = CalType.SIGN_PLUS
+    // かっこ"()"内の計算を実施
+    @Throws(CalException::class)
+    private fun calBracket() {
+        // 左かっこ"("の位置
+        val posL = tokenLst.indexOf("(")
+        // 右かっこ")"の位置
+        var posR = -1
+        // トークンの長さ
+        val size = tokenLst.size
+        // かっこのレベル
+        // -------------------------------------
+        //   (1+2)
+        //      =>
+        //      かっこレベルは0
+        // -------------------------------------
+        //   ((1+2)*(3+4))
+        //      =>
+        //      一番外側のかっこレベルは0
+        //      (1+2)と(3+4)のかっこレベルは1
+        var level = 0
+
+        // 左かっこ"("に対応する右かっこ")"を探す
+        (posL+1 until size).forEach bracket@{
+            when (tokenLst[it]) {
+                ")" -> {
+                    // 同一レベルの右かっこがみつかった場合
+                    if (level == 0) {
+                        posR = it
+                        return@bracket
                     }
-                    // 上記以外
+                    // 異なるレベルの右かっこがみつかった場合
+                    // レベルを下げる
                     else {
-                        prevCalType = CalType.OPERATOR_PLUS
+                        level--
                     }
                 }
-                // マイナス符号
-                ( s == "-" ) -> {
-                    // 式の先頭 or カッコ内の先頭
-                    if ( ( prevCalType == CalType.YET ) or ( prevCalType == CalType.BRACKET_LEFT ) ) {
-                        prevCalType = CalType.SIGN_MINUS
-                    }
-                    // 上記以外
-                    else {
-                        prevCalType = CalType.OPERATOR_MINUS
-                    }
-                }
-                // 左かっこ"("
-                ( s == "(" ) -> {
-                    prevCalType = CalType.BRACKET_LEFT
-                }
-                // 右かっこ")"
-                ( s == ")" ) -> {
-                    prevCalType = CalType.BRACKET_RIGHT
-                }
-                // 上記以外
-                else -> {
-                    val num = s.toDoubleOrNull()
-                    if ( num != null ) {
-                        // 1つ前が符号の場合マージし、
-                        // トークンリストから符号を削除
-                        if ( prevCalType == CalType.SIGN_PLUS ) {
-                            tokenLst.removeAt(index-1)
-                        }
-                        else if ( prevCalType == CalType.SIGN_MINUS ) {
-                            tokenLst[index] = (-1 * num).toString()
-                            tokenLst.removeAt(index-1)
-                        }
-                        prevCalType = CalType.NUM
-                    }
-                    else {
-                        prevCalType = CalType.UNKNOWN
-                    }
+                "(" -> {
+                    // 左かっこが見つかった場合
+                    // レベルを上げる
+                    level++
                 }
             }
         }
-        */
+
+        // 左かっこ"("右かっこ")"ともに見つかれば、
+        // 該当部分を配列から取り出して、解析処理に回す
+        if ( ( posL != -1 ) and ( posR != -1 ) and (posL < posR) ) {
+            val tmpTokenLst = mutableListOf<String>()
+            // かっこ"()"内の式をテンポラリ配列に詰め込む
+            (posL+1..posR-1).forEach { tmpTokenLst.add(tokenLst[it]) }
+            // かっこ"()"で囲まれた配列を現在のトークンリストから外す
+            (posL..posR).forEach { tokenLst.removeAt(posL) }
+
+            // かっこ"()"内の式の値を取得
+            val calCtx = ContextCal(tmpTokenLst)
+            Log.d(javaClass.simpleName, "bra:eq[${calCtx.toEquation()}]")
+            val num = calCtx.execute()
+            Log.d(javaClass.simpleName, "bra:num[$num]")
+
+            // かっこ"()"内の式の値を現在のトークンリストに入れ込む
+            tokenLst.add(posL,num.toString())
+            Log.d(javaClass.simpleName, "modify:eq[${toEquation()}]")
+
+            // まだ、かっこがあるかもしれないので、
+            // 再度かっこ"()"内の計算を実施
+            calBracket()
+        }
+        // 左かっこ"("右かっこ")"ともに見つからなければ、
+        // 何もせず処理を終了
+        else if ( ( posL == -1 ) and ( posR == -1 ) ) {
+        }
+        // 左かっこ"("しかみつからない場合エラー
+        // 右かっこ")"しかみつからない場合エラー
+        else {
+            throw CalException("Bracket'()' should be pair")
+        }
     }
+
+    // 累乗計算を実施
+    // -----------------------
+    // 累乗は、後ろ優先っぽい
+    // -----------------------
+    // 2^2 = 4
+    // 2^2^2 = 16
+    // 2^2^3 = 256
+    @Throws(CalException::class)
+    private fun calPow() {
+        // 累乗記号(^)を後ろから見つける
+        val pos = tokenLst.lastIndexOf("^")
+
+    }
+
 
     // 乗除算を実施
     // -------------------------------
